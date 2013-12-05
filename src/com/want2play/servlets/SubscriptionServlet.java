6ad686b3cc05a0b02a23928f063034c2a4e804be
want2play-nantes <adm.want2play@gmail.com>
2013-12-05 16:19:05 +0100

    ajout des subscriptions

--- src/com/want2play/servlets/SubscriptionServlet.java
+++ src/com/want2play/servlets/SubscriptionServlet.java
@@ -1,11 +1,18 @@
 package com.want2play.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.want2play.core.Subscriptions;
+import com.want2play.datastore.DatastoreController;
 
 @SuppressWarnings("serial")
 public class SubscriptionServlet extends HttpServlet {
@@ -14,6 +21,17 @@ public class SubscriptionServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException
 	{
+		
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+        PrintWriter out = resp.getWriter();
+		
+        Subscriptions subs ;
+		if (DatastoreController.isSubscribedUser(user)){
+			subs = DatastoreController.getSubscriptionsByUser(user);
+		}else{ subs = new Subscriptions(user);}
+			 
+		req.setAttribute("subs",subs);
 		getServletContext().getRequestDispatcher("/WEB-INF/subscription.jsp").forward(req, resp);
 	}
 }
